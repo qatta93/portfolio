efcd37beb909344d8403de3b0f1d1032d381dc4f
Patrycja Panasiuk <panasiuk.patrycja@gmail.com>
2020-01-02 10:29:26 +0100

    before browsers compatibility changes

--- project.js
+++ project.js
@@ -1,7 +1,7 @@
-var btn = document.getElementsByClassName("intro__btn");
-var intro = document.getElementsByClassName("intro");
-var home = document.getElementsByClassName("home");
+// var btn = document.getElementsByClassName("intro__btn");
+// var intro = document.getElementsByClassName("intro");
+// var home = document.getElementsByClassName("home");
 
-btn.addEventListener("click", function(){
-    intro
-});
+// btn.addEventListener("click", function(){
+//     document.getElementsByClassName('home');
+// });
